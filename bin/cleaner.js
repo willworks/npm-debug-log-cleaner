d41fef143f0e6e526878d4f2208ecf614c62e88d
willworks <scnuzhonghanjin@gmail.com>
2017-07-20 21:20:18 +0800

    feat: check .gitignore exist

--- bin/cleaner.js
+++ bin/cleaner.js
@@ -2,7 +2,23 @@
 
 'use strict'
 
+const fs = require("fs")
+const os = require("os")
 const exec = require('child_process').exec
 const CLEAN_CMD = 'rm ./npm-debug*'
 
-exec(CLEAN_CMD, () => { console.log('done...') });
+fs.open('.gitignore', 'wx', (err, fd) => {
+  if (err) {
+    if (err.code === 'EEXIST') {
+      console.error('.gitignore already exists');
+      return;
+    }
+    throw err;
+  }
+  fs.appendFile('.gitignore', 'npm-debug*' + os.EOL, (err) => {
+    if (err) throw err;
+    console.log('.gitignore has been saved!');
+  });
+});
+
+exec(CLEAN_CMD, () => { console.log('clean done!') });
--- test/test.js
+++ test/test.js
@@ -7,8 +7,8 @@ const exec = require('child_process').exec
 const CREATE_CMD = 'touch npm-debug.log.test'
 const CLEAN_CMD = 'npm-debug-log-cleaner'
 
-exec(CREATE_CMD, () => { console.log('done...') });
+exec(CREATE_CMD, () => { console.log('create done...') });
 setTimeout(() => {
   console.log('wait...')
-  exec(CLEAN_CMD, () => { console.log('done...') });
-}, 2000);
+  exec(CLEAN_CMD, () => { console.log('clean done...') });
+}, 2500);
